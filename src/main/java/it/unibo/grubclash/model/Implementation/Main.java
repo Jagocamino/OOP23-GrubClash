2157a32bee8674190d054a912f192ee46e0b2ddd
GiorgiaPatera <giorgia.patera@studio.unibo.it>
2024-06-01 18:49:32 +0200

    main

---
+++ src/main/java/it/unibo/grubclash/model/Implementation/Main.java
@@ -0,0 +1,12 @@
+package it.unibo.grubclash.model.Implementation;
+
+import it.unibo.grubclash.view.Implementation.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Menu();
+    }
+
+}
