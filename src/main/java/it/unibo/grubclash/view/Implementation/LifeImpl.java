cb6afb4ff826cc5f4026a12b7c4f1a2943e78108
GiorgiaPatera <giorgia.patera@studio.unibo.it>
2024-06-02 16:47:55 +0200

    piccole modifiche alla classe LifeImpl

--- src/main/java/it/unibo/grubclash/view/Implementation/LifeImpl.java
+++ src/main/java/it/unibo/grubclash/view/Implementation/LifeImpl.java
@@ -1,33 +1,25 @@
 package it.unibo.grubclash.view.Implementation;
-import it.unibo.grubclash.controller.Application_Programming_Interface.GrubPanelInter;
 import it.unibo.grubclash.controller.Implementation.GrubPanel;
 import it.unibo.grubclash.controller.Implementation.Player;
 import it.unibo.grubclash.view.Application_Programming_Interface.Life;
 import it.unibo.grubclash.view.Implementation.LifeImpl;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.util.Optional;
 
 public class LifeImpl implements Life {
 
-    BufferedImage life20, life40, life60, life80, life100;
-
-    int playerCount;
-    GrubPanelInter grubPanel;
-    Graphics2D g2d;
-    String color_game = "#EF7B10";
+    private String color_game = "#EF7B10";
     private final Font snapITCFont;
     private final Player player;
 
     //vita di ogni giocatore che va da 10 a 0
-    int life;
+    private int life;
     private final int value=2;
     private LifeDrawingImpl drawingStrategy;
 
     public LifeImpl (GrubPanel grubPanel,Player player,LifeDrawingImpl drawingStrategy){
-        this.playerCount= grubPanel.playerCount;
-        this.grubPanel=grubPanel;
+
         this.life=10;
         this.player=player;
         this.drawingStrategy = drawingStrategy;
@@ -38,7 +30,6 @@ public class LifeImpl implements Life {
     @Override 
     public void draw(Graphics2D g2d){
         
-        this.g2d = g2d;
         g2d.setFont(snapITCFont);
         g2d.setColor(Color.decode(color_game));
 
