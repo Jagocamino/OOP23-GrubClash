a2447f034dfe197a3c61bd47aa73fbe434d8b468
GiorgiaPatera <giorgia.patera@studio.unibo.it>
2024-06-02 16:36:16 +0200

    sistemata classe menu per aderire a un pattern

--- src/main/java/it/unibo/grubclash/view/Implementation/Menu.java
+++ src/main/java/it/unibo/grubclash/view/Implementation/Menu.java
@@ -4,7 +4,7 @@ import it.unibo.grubclash.controller.Implementation.MapBuilder;
 import it.unibo.grubclash.view.Application_Programming_Interface.FrameManager;
 
 import javax.swing.*;
-import javax.swing.JFrame;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -16,25 +16,12 @@ public class Menu extends JFrame{
     private static final FrameManager frameManager = FrameManagerImpl.getInstance();
 
     final char FS = File.separatorChar;
+    JComboBox<Integer> playerSelect;
 
     public Menu() {
 
-        //Creazione del frame
-        JFrame frameMenu = new JFrame();
-        frameMenu.setTitle("GrubClash");
-        frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameMenu.setSize(frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
-        frameMenu.setResizable(false);
-        frameMenu.setLayout(new BorderLayout());
-
-        //visto che ho creato l'interfaccia di FrameManager e non posso mettere i metodi statici creo il frame Fanager e gli setto l'icono FM
-        frameManager.setIcon(frameMenu);
-
-        //Pannello menù
-        final JPanel panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
-        frameMenu.add(panel);
+        JFrame frameMenu = createMenuFrame();
+        JPanel panel = createMenuPanel(frameMenu);
 
         //Impostazione dello sfondo del menu
         ImageIcon image = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "menu_bg.jpg");
@@ -42,92 +29,58 @@ public class Menu extends JFrame{
         menu_bg.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
 
         //Creazione del bottone play
-        final JButton playButton = new JButton();
+        JButton playButton = createPlayButton(frameMenu);
+
+        playerSelect = new JComboBox<>(new Integer[]{2, 3, 4, 5});
+        playerSelect.setBounds(frameManager.getWindowWidth().get()-950, frameManager.getWindowHeight().get()-455, 200, 30);
+
+        panel.add(playButton);
+        panel.add(playerSelect);
+        panel.add(menu_bg);
+        frameMenu.setVisible(true);
+    }
+
+    protected JFrame createMenuFrame() {
+        JFrame frameMenu = new JFrame();
+        frameMenu.setTitle("GrubClash");
+        frameMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMenu.setSize(frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
+        frameMenu.setResizable(false);
+        frameMenu.setLayout(new BorderLayout());
+        frameManager.setIcon(frameMenu);
+        return frameMenu;
+    }
+
+    protected JPanel createMenuPanel(JFrame frameMenu) {
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
+        frameMenu.add(panel);
+        return panel;
+    }
+
+    protected JButton createPlayButton(JFrame frameMenu) {
+        JButton playButton = new JButton();
         playButton.setBorderPainted(false);
-        playButton.setBounds(frameManager.getWindowWidth().get()-1150,frameManager.getWindowHeight().get()-350, 200, 200);
+        playButton.setBounds(frameManager.getWindowWidth().get() - 1150, frameManager.getWindowHeight().get() - 350, 200, 200);
 
         ImageIcon originalIcon = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play.png");
         Image originalImage = originalIcon.getImage();
-
         ImageIcon scaledIcon = new ImageIcon(originalImage.getScaledInstance(playButton.getWidth(), playButton.getHeight(), Image.SCALE_SMOOTH));
         playButton.setIcon(scaledIcon);
 
-        JComboBox<Integer> playerSelect = new JComboBox<>(new Integer[]{2, 3, 4, 5});
-        playerSelect.setBounds(frameManager.getWindowWidth().get()-950, frameManager.getWindowHeight().get()-455, 200, 30);
-
-        // Evento scatenato al click del bottone
         playButton.addActionListener(e -> {
-
             frameMenu.dispose();
-            JFrame frameMappa = new JFrame();
-            frameMappa.setTitle("GrubClash");
-            frameMappa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frameMappa.setSize(frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
-            frameMappa.setResizable(false);
-            frameMappa.setLayout(new BorderLayout());
-            frameManager.setIcon(frameMappa);
-
-            final JPanel panelMappa = new JPanel();
-            panelMappa.setLayout(null);
-            panelMappa.setBackground(Color.WHITE);
-            panelMappa.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
-
-            frameMappa.add(panelMappa);
-
-            ImageIcon mappa = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "grubRules.png");
-            JLabel mappa_label = new JLabel(mappa);
-            mappa_label.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
-
-
-            final JButton play2 = new JButton();
-            play2.setBorderPainted(false);
-            play2.setBounds((frameManager.getWindowWidth().get()/2) - 100,0,100, 100);
-            ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play2.png");
-            play2.setIcon(originalIcon2);
-
-            play2.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play_on_hover2.png");
-                    play2.setIcon(originalIcon2);
-                }
-    
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play2.png");
-                    play2.setIcon(originalIcon2);
-                } 
-            });
-
-            panelMappa.add(play2);
-            panelMappa.add(mappa_label);
-
+            JFrame frameMappa = createMapFrame();
+            createMapPanel(frameMappa);
             frameMappa.setVisible(true);
-
-            play2.addActionListener(b ->{
-                frameMappa.dispose();
-                Object player = playerSelect.getSelectedItem();
-                int playerCount=0;
-                if (player != null) {
-                    playerCount = (int) player;
-                }
-                else{
-                    System.out.println("Errore nella selezione del player");
-                    System.exit(1);
-                }
-                new MapBuilder(playerCount);
-
-            });
-
         });
 
-        // Effetto sul bottone al passaggio del mouse
         playButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
                 ImageIcon originalIcon = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play_on_hover.png");
                 Image originalImage = originalIcon.getImage();
-
                 ImageIcon scaledIcon = new ImageIcon(originalImage.getScaledInstance(playButton.getWidth(), playButton.getHeight(), Image.SCALE_SMOOTH));
                 playButton.setIcon(scaledIcon);
             }
@@ -136,15 +89,72 @@ public class Menu extends JFrame{
             public void mouseExited(MouseEvent e) {
                 ImageIcon originalIcon = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play.png");
                 Image originalImage = originalIcon.getImage();
-
                 ImageIcon scaledIcon = new ImageIcon(originalImage.getScaledInstance(playButton.getWidth(), playButton.getHeight(), Image.SCALE_SMOOTH));
                 playButton.setIcon(scaledIcon);
             }
         });
 
-        panel.add(playButton);
-        panel.add(playerSelect);
-        panel.add(menu_bg);
-        frameMenu.setVisible(true);
+        return playButton;
+    }
+
+    protected JFrame createMapFrame() {
+        JFrame frameMappa = new JFrame();
+        frameMappa.setTitle("GrubClash");
+        frameMappa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMappa.setSize(frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
+        frameMappa.setResizable(false);
+        frameMappa.setLayout(new BorderLayout());
+        frameManager.setIcon(frameMappa);
+        return frameMappa;
+    }
+
+    protected JPanel createMapPanel(JFrame frameMappa) {
+        JPanel panelMappa = new JPanel();
+        panelMappa.setLayout(null);
+        panelMappa.setBackground(Color.WHITE);
+        panelMappa.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
+
+        ImageIcon mappa = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "grubRules.png");
+        JLabel mappa_label = new JLabel(mappa);
+        mappa_label.setBounds(0, 0, frameManager.getWindowWidth().get(), frameManager.getWindowHeight().get());
+
+        JButton play2 = new JButton();
+        play2.setBorderPainted(false);
+        play2.setBounds((frameManager.getWindowWidth().get() / 2) - 100, 0, 100, 100);
+        ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play2.png");
+        play2.setIcon(originalIcon2);
+
+        play2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play_on_hover2.png");
+                play2.setIcon(originalIcon2);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                ImageIcon originalIcon2 = new ImageIcon("src" + FS + "main" + FS + "resources" + FS + "menu" + FS + "play2.png");
+                play2.setIcon(originalIcon2);
+            }
+        });
+
+        play2.addActionListener(b -> {
+            frameMappa.dispose();
+            Object player = playerSelect.getSelectedItem();
+            int playerCount = 0;
+            if (player != null) {
+                playerCount = (int) player;
+            } else {
+                System.out.println("Errore nella selezione del player");
+                System.exit(1);
+            }
+            new MapBuilder(playerCount);
+        });
+
+        panelMappa.add(play2);
+        panelMappa.add(mappa_label);
+        frameMappa.add(panelMappa);
+
+        return panelMappa;
     }
 }
